35bb5cf181acd1ea5322cf1a57d510ab82d31dbb
Rahim-sarker <mhrahim08@gmail.com>
2022-11-30 10:57:45 +0600

    Server Complete

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,38 +10,165 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri =
+  "mongodb+srv://rahim:N8oBW27yEJ4qSfTw@cluster0.jlae62x.mongodb.net/?retryWrites=true&w=majority";
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.twtll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-async function run(){
-    try{
-        await client.connect();
-        const serviceCollection = client.db('doctors_portal').collection('services');
-
-        app.get('/service', async(req, res) =>{
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-        })
-
-
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbiden access" });
     }
-    finally{
+    req.decoded = decoded;
+    next();
+  });
+}
 
-    }
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
+
+    const bookingCollection = client.db("doctors_portal").collection("booking");
+    const userCollection = client.db("doctors_portal").collection("users");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/user", async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    });
+
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    //make admin api
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requestAccount = await userCollection.findOne({ email: requester });
+
+      if (requestAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    });
+
+    //User Upsert into db Start
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+          expiresIn: "1h",
+        }
+      );
+      res.send({ result, token });
+    });
+    //User Upsert into db End
+
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+
+      //step-1 get all services
+
+      const services = await serviceCollection.find().toArray();
+
+      //step-2 get the booking of that day
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      //step 3 for each service , find the booking for that service
+
+      services.forEach((service) => {
+        const serviceBookings = bookings.filter(
+          (book) => book.treatment === service.name
+        );
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        service.slots = available;
+      });
+
+      res.send(services);
+    });
+
+    //Finding user specific data start
+
+    app.get("/booking", verifyJWT, async (req, res) => {
+      const patient = req.query.patient;
+      const decodedEmail = req.decoded.email;
+      if (patient === decodedEmail) {
+        const query = { patient: patient };
+        const bookings = await bookingCollection.find(query).toArray();
+        return res.send(bookings);
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+    });
+    //Finding user specific data end
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment, //74-2-3
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    });
+  } finally {
+  }
 }
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello From Doctor Uncle!')
-})
+app.get("/", (req, res) => {
+  res.send("Rahim Sarker's Public Health Server");
+});
 
 app.listen(port, () => {
-  console.log(`Doctors App listening on port ${port}`)
-})
+  console.log(`Doctors App listening on port ${port}`);
+});
